5ffc3bc90d88d5231567fc708869588be4c7422f
Sergio-qo <sergio.qo000@gmail.com>
2026-06-15 23:25:47 +0200

    Creando backend, el login debe de comparar con contraseñas encriptadas en bd

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['password'] != '':
+        return jsonify({'message': 'Invalid credentials'}), 401
+    else:
+        return jsonify({'message': 'Login successful!'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
