2ecc4949ae2b4e7184073c41931aa8e71baf4d32
Sergio <sergio.qo000@gmail.com>
2026-06-22 11:44:59 +0200

    Modificación en rutas y servicios

--- app/front_office/routes.py
+++ app/front_office/routes.py
@@ -7,10 +7,17 @@ from . import front_bp
 
 @front_bp.route('/')
 def home():
-    services = [{},{}]
-    return render_template('front/index.html', services=services)
     return redirect(url_for('front_office.login'))
+
+@front_bp.route('/services/', defaults={'id': None})
+@front_bp.route('/services/<id>')
+def services(id):
+    if id != None:
+        return "Servicio: " + str(id)
+    else:
+        services = [{'title': 'Panel de Automatizaciones', 'description': 'Panel de Automatizaciones', 'id': 1},{'title': 'Panel de Automatizaciones', 'description': 'Panel de Automatizaciones', 'id': 2}]
+        return render_template('front/index.html', services=services)
 
 @front_bp.route('/login')
 def login():
-    return render_template('front/index.html')
+    return render_template('front/login.html')
